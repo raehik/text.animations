42c5825b9c4e616308d8b285b3cb8843eece0c68
Ben Orchard <thefirstmuffinman@gmail.com>
2013-09-22 17:38:50 +0100

    remove old functions

--- text/animations/Speaker.java
+++ text/animations/Speaker.java
@@ -27,16 +27,7 @@ public class Speaker {
 			}
 		}
 	}
-	
-	public void sayOld(String line) {
-		sayWithSpecifiedPauses(line);
-	}
-	
-	private void sayChar(char c) {
-		System.out.print(c);
-		sleepFor(this.pause);
-	}
-	
+
 	private int findPause(String substr) {
 		if (substr.length() < 2) { return -1; }
 		if (substr.charAt(1) == '^') {
@@ -45,30 +36,13 @@ public class Speaker {
 		else { return 0; } // regular char
 	}
 	
-	private void sayWithSpecifiedPauses(String line) {
-		for (int i=0; i<line.length(); i++) {
-			System.out.print(line.charAt(i)); // print char
-			int nextChar = checkNextChar(line, i); // do tests on next char
-			if (nextChar == 2) { continue; } // no pause on last char
-			if (nextChar == 1) { // if delimiter next
-				int num_between = numOfBetweenChars(line.substring(i+2)); // send substring starting with first number
-				String cust_sleep_s = line.substring(i+2,i+num_between+1);
-				int cust_sleep = Integer.parseInt(cust_sleep_s);
-				sleepFor(cust_sleep);
-				i = i + num_between + 1;
-			}
-			else { sleepFor(this.pause); }
-		}
+	private int numOfBetweenChars(String str) {
+		return str.indexOf('^')+1;
 	}
 	
-	private int checkNextChar(String line, int i) {
-		if (i == line.length()-1) { // if is last char
-			return 2;
-		}
-		else if (line.charAt(i+1) == '^') { // if delimiter found
-			return 1;
-		}
-		return 0; // regular char
+	private void sayChar(char c) {
+		System.out.print(c);
+		sleepFor(this.pause);
 	}
 	
 	
@@ -78,9 +52,5 @@ public class Speaker {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-	}
-
-	private int numOfBetweenChars(String str) {
-		return str.indexOf('^')+1;
 	}
 }
